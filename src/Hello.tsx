1d199afb8a3eba55c9f44e4b4390f9329000400d
omkar342 <omkarjadhav095@gmail.com>
2024-07-14 10:42:07 +0530

    removed linting error.

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import { useEffect } from 'react'
 import { useUser, UserContextType } from '../src/contexts/userContext'; 
 import { useNavigate } from 'react-router-dom';
 import { toast } from "react-hot-toast";
